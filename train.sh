1b645d980c0f943ca78d324bf5140fc2092e799a
Jollian <109514873+jollianreap@users.noreply.github.com>
2024-04-03 01:10:44 +0300

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
 
-# shellcheck disable=SC1073
-cwd=$(pwd)
-poems=$cwd'/data/poems/'
-#amount=$(ls $poems | wc -l)
-
-
-# shellcheck disable=SC2045
-
-path_to_poems=$cwd'/data/all_poems.csv'
-# shellcheck disable=SC2154
-$(cd lyrics-generator_copy && python3 -m lyrics.train --songdata-file $path_to_poems --artists "*")
-
-#echo $cwd
